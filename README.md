f7cc531b4411259e2378b58001745941b8f2a9c1
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2017-03-06 15:38:31 -0500

    added ignores and pachages

---
+++ README.md
@@ -0,0 +1,4 @@
+# Bcrypt Practice
+
+This is a practice session held in class for bcrypt with Tims Gardner
+
---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+// requiring environmental keys
+require('dotenv').config({ silent: true });
+
+const express = require('express'),
+      app = express(),
+      port = process.env.PORT || 3000,
+      morgan = require('morgan'),
+      mustache = require('mustache'),
+      bodyParser = require('body-parser')
+
+// setting up views
+app.engine('html', mustache());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+// public folder
+app.use(express.static(__dirname + '/public'));
+
+// setting up the logger colors
+app.use(morgan('dev'));
+
+// setting up body parser
+app.use(bodyParser)
+
+app.listen(port, ()=> {
+  console.log(`dumplings on ${port}`);
+})
